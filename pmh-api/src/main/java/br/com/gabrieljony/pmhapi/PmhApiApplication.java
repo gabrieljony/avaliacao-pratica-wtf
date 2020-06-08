abd14c35680eb352a9eb1f191aff48318e8c8637
Gabriel Jony <gabjony@gmail.com>
2020-06-08 10:32:08 -0300

    Create api do Sistema de Atendimento

---
+++ pmh-api/src/main/java/br/com/gabrieljony/pmhapi/PmhApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieljony.pmhapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmhApiApplication.class, args);
+	}
+
+}
